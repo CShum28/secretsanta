214b96834ec894283cfd92fdbc9b3afd8a1623e3
Clement <clement.shum128@gmail.com>
2023-12-21 18:21:05 -0500

    updated code

---
+++ routes/insertSantaListGame.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+function shuffleArray(array) {
+  let n = array.length;
+  for (let i = 0; i < n - 1; i++) {
+    // Choose a random index from the remaining elements, excluding the current one (i)
+    let j = i + Math.floor(Math.random() * (n - i - 1)) + 1;
+
+    // Swap the elements at indices i and j
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+router.post("/", (req, res) => {
+  const players = req.body;
+  const santas = [...req.body];
+  const secret = [];
+
+  const shuffled = shuffleArray(santas);
+
+  //   console.log("shuffled: ", shuffled);
+
+  for (let i = 0; i < players.length; i++) {
+    secret.push({ Recipient: players[i], Santa: santas[i] });
+  }
+  console.log(secret);
+  res.send(secret);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// const helloWorld = require("./routes/helloWorld");
+// const getSantaListById = require("./routes/getSantaListById");
+const insertSantaListGame = require("./routes/insertSantaListGame");
+
+// app.use("/", helloWorld);
+// app.use("/get-game", getSantaListById);
+app.use("/insert-list", insertSantaListGame);
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
