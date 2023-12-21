e491e29af709d21bacb37e1b7a7d72199112ca57
Clement <clement.shum128@gmail.com>
2023-12-21 18:39:03 -0500

    hello world

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 
 app.use(express.json());
 
-// const helloWorld = require("./routes/helloWorld");
+const helloWorld = require("./routes/helloWorld");
 // const getSantaListById = require("./routes/getSantaListById");
 const insertSantaListGame = require("./routes/insertSantaListGame");
 
-// app.use("/", helloWorld);
+app.use("/", helloWorld);
 // app.use("/get-game", getSantaListById);
 app.use("/insert-list", insertSantaListGame);
 
